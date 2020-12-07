76b0e379f5286c6570a55e99e70eace7209fc295
sejin <sejila@insightinfo.kr>
2020-12-07 18:10:04 +0900

    Add Main-Class attribute to manifest

--- gradle/building-java-applications-sample/app/build.gradle.kts
+++ gradle/building-java-applications-sample/app/build.gradle.kts
@@ -36,3 +36,11 @@ tasks.test {
     // Use junit platform for unit tests.
     useJUnitPlatform()
 }
+
+tasks.jar {
+    manifest {
+        attributes(mapOf("Implementation-Title" to project.name,
+                "Implementation-Version" to project.version,
+                "Main-Class" to "sample.App"))
+    }
+}
