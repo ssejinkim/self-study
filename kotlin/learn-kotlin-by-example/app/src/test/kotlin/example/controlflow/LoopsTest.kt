d26aaf265a0d917dbd20d2e4f2c0b6f6b317ca6e
sejin <sejila@insightinfo.kr>
2020-12-10 09:55:36 +0900

    Add Loops test

---
+++ kotlin/learn-kotlin-by-example/app/src/test/kotlin/example/controlflow/LoopsTest.kt
@@ -0,0 +1,69 @@
+package example.controlflow
+
+import org.assertj.core.api.BDDAssertions.then
+import org.junit.jupiter.api.Test
+
+class LoopsTest {
+
+    @Test
+    fun `for`() {
+        val cakes = listOf("carrot", "cheese", "chocolate")
+
+        for (cake in cakes) {
+            println("Yummy, it's a $cake cake!")
+        }
+
+        then(cakes)
+                .contains("carrot", "cheese", "chocolate")
+    }
+
+    @Test
+    fun `while and do-while`() {
+        fun bakeACake() = println("Bake a Cake")
+        fun eatACake() = println("Eat a Cake")
+
+        var cakesBaked = 0
+        var cakesEaten = 0
+
+        while (cakesBaked < 5) {
+            bakeACake()
+            cakesBaked++
+        }
+
+        then(cakesBaked).isEqualTo(5)
+
+        do {
+            eatACake()
+            cakesEaten++
+        } while (cakesEaten < cakesBaked)
+
+        then(cakesEaten).isEqualTo(5)
+    }
+
+    @Test
+    fun `Iterators`() {
+        class Animal(val name: String)
+
+        class Zoo(val animals: List<Animal>) {
+            operator fun iterator(): Iterator<Animal> {
+                return animals.iterator()
+            }
+        }
+
+        val zoo = Zoo(listOf(Animal("zebra"), Animal("lion")))
+
+        for (animal in zoo) {
+            println("Watch out, it's a ${animal.name}")
+        }
+
+        val iterator = zoo.iterator()
+
+        while (iterator.hasNext()) {
+            println("Watch out, it's a ${iterator.next().name}")
+        }
+
+        then(zoo.animals)
+                .extracting("name")
+                .contains("zebra", "lion")
+    }
+}
