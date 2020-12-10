7e1d6b20593e616be64565c3e208aa98f90ede74
sejin <sejila@insightinfo.kr>
2020-12-11 08:37:50 +0900

    Add Apply test

--- kotlin/learn-kotlin-by-example/app/src/test/kotlin/example/scopeFuctions/ApplyTest.kt
+++ kotlin/learn-kotlin-by-example/app/src/test/kotlin/example/scopeFuctions/ApplyTest.kt
@@ -1,4 +1,27 @@
 package example.scopeFuctions
 
+import org.assertj.core.api.BDDAssertions.then
+import org.junit.jupiter.api.Test
+
 class ApplyTest {
+
+    @Test
+    fun `apply executes a block of code on an object and returns the object itself  Inside the block, the object is referenced by this`() {
+        data class Person(var name: String, var age: Int, var about: String) {
+            constructor() : this("", 0, "")
+        }
+
+        val jake = Person()
+
+        jake.apply {
+            name = "Jake"
+            age = 30
+            about = "Android Developer"
+        }
+
+        then(jake.name).isEqualTo("Jake")
+        then(jake.age).isEqualTo(30)
+        then(jake.about).isEqualTo("Android Developer")
+
+    }
 }
