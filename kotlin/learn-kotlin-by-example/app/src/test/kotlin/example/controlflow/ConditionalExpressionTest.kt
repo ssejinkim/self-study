0add797d157af10f819250bdb6dc6dc2841fbc47
sejin <sejila@insightinfo.kr>
2020-12-10 10:31:57 +0900

    Add Conditional Expression test

---
+++ kotlin/learn-kotlin-by-example/app/src/test/kotlin/example/controlflow/ConditionalExpressionTest.kt
@@ -0,0 +1,14 @@
+package example.controlflow
+
+import org.assertj.core.api.BDDAssertions.then
+import org.junit.jupiter.api.Test
+
+class ConditionalExpressionTest {
+
+    @Test
+    fun `Conditional Expression`() {
+        fun max(a: Int, b: Int) = if (a > b) a else b
+
+        then(max(99, -42)).isEqualTo(99)
+    }
+}
