05a224a8e1d5cf9fac20045dd627047bcf8355ff
sejin <sejila@insightinfo.kr>
2020-12-10 13:43:58 +0900

    Add Lambda Functions test

---
+++ kotlin/learn-kotlin-by-example/app/src/test/kotlin/example/functional/LambdaFunctionsTest.kt
@@ -0,0 +1,24 @@
+package example.functional
+
+import org.assertj.core.api.BDDAssertions.then
+import org.junit.jupiter.api.Test
+
+class LambdaFunctionsTest {
+
+    @Test
+    fun `Lambda Functions`() {
+        val upperCase1: (String) -> String = { str: String -> str.toUpperCase() }
+        val upperCase2: (String) -> String = { str -> str.toUpperCase() } // Type inference inside lambda
+        val upperCase3 = { str: String -> str.toUpperCase() } // Type inference outside lambda
+//        val upperCase4 = { str -> str.toUpperCase() } // You cannot do both together
+        val upperCase5: (String) -> String = { it.toUpperCase() }
+        val upperCase6: (String) -> String = String::toUpperCase // lambda consists of a single function call, you may use function pointers (::) .
+
+        then(upperCase1("lowerCase")).isEqualTo("LOWERCASE")
+        then(upperCase2("lowerCase")).isEqualTo("LOWERCASE")
+        then(upperCase3("lowerCase")).isEqualTo("LOWERCASE")
+        then(upperCase5("lowerCase")).isEqualTo("LOWERCASE")
+        then(upperCase6("lowerCase")).isEqualTo("LOWERCASE")
+    }
+
+}
