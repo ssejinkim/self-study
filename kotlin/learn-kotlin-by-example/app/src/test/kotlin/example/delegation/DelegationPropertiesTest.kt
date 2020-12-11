c13d002b4aed28bec8333ce6cc289de918514a11
sejin <sejila@insightinfo.kr>
2020-12-11 10:49:24 +0900

    Add Delegation Properties test

---
+++ kotlin/learn-kotlin-by-example/app/src/test/kotlin/example/delegation/DelegationPropertiesTest.kt
@@ -0,0 +1,11 @@
+package example.delegation
+
+import org.junit.jupiter.api.Test
+
+class DelegationPropertiesTest {
+
+    @Test
+    fun `Delegation Properties`() {
+        TODO("Not yet implemented")
+    }
+}
