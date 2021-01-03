2871fc7d569b1a5a1b67d9187056dc1b68931cec
sejinkim <foraiz86@gmail.com>
2021-01-03 19:45:24 +0900

    logic/if-blocks

---
+++ svelte/tutorial-with-sapper/src/routes/logic/if-blocks.svelte
@@ -0,0 +1,21 @@
+<script>
+
+    let user = { loggIn: false};
+
+    function toggle() {
+        user.loggIn = !user.loggIn;
+    }
+
+</script>
+
+{#if user.loggIn}
+<button on:click={toggle}>
+    Log out
+</button>
+{/if}
+
+{#if !user.loggIn}
+<button on:click={toggle}>
+    Log in
+</button>
+{/if}
