b7591c9974f78bdfe76e0df0e74fe3d2ebfc6c13
sejinkim <foraiz86@gmail.com>
2021-01-04 07:10:15 +0900

    bindings/textarea-inputs

---
+++ svelte/tutorial-with-sapper/src/routes/bindings/textarea-inputs.svelte
@@ -0,0 +1,15 @@
+<script>
+    import marked from 'marked'
+    let value = 'Some words are *italic*, soem are **bold**';
+</script>
+
+<style>
+    textarea {
+        width: 100%;
+        height: 200px;
+    }
+</style>
+
+<textarea bind:value></textarea>
+
+{@html marked(value)}
