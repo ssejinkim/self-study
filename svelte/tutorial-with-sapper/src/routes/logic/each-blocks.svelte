7607d022f2b8befb040e6e20ad5a5edc9690c77a
sejinkim <foraiz86@gmail.com>
2021-01-03 21:50:40 +0900

    logic/each-blocks

---
+++ svelte/tutorial-with-sapper/src/routes/logic/each-blocks.svelte
@@ -0,0 +1,30 @@
+<script>
+    let cats = [
+        { id: "ghpqwhpt", name: 'Keyboard Cat'},
+        { id: "asdgqw23", name: 'Maru'},
+        { id: "nhguashi", name: 'Henri The Existential Cat'},
+    ]
+</script>
+
+<h1>The Famous Cats of Youtube</h1>
+
+
+<ul>
+    {#each cats as cat, i}
+    <li>
+    <a target="_blank" href="https://www.youtube.com/watch?v={cat.id}">
+        {i + 1 } : {cat.name}
+    </a>
+    </li>
+    {/each}
+</ul>
+
+<ul>
+    {#each cats as {id, name}, i}
+    <li>
+    <a target="_blank" href="https://www.youtube.com/watch?v={id}">
+        {i + 1 } : {name}
+    </a>
+    </li>
+    {/each}
+</ul>
