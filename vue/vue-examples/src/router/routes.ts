ffbf4a7759a124ca5adbbc852ed2bd98d9d9639c
Sejin Kim <sejinkim2022@gmail.com>
2022-07-21 08:41:30 +0900

    Add Conditional Rendering example

--- vue/vue-examples/src/router/routes.ts
+++ vue/vue-examples/src/router/routes.ts
@@ -3,39 +3,45 @@ import DeclarativeRenderingView from '@/views/DeclarativeRendering.vue'
 import AttributeBindingsView from '@/views/AttributeBIndings.vue'
 import EventListenersView from '@/views/EventListeners.vue'
 import FormBindingsView from '@/views/FormBindings.vue'
+import ConditionalRenderingView from '@/views/ConditionalRendering.vue'
 
-export const routes =  [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/DeclarativeRendering',
-      name: 'Declarative Rendering',
-      component: DeclarativeRenderingView 
-    },
-    {
-      path: '/AttributeBindings',
-      name: 'Attribute Bindings',
-      component: AttributeBindingsView
-    },
-    {
-      path: '/EventListeners',
-      name: 'Event Listeners',
-      component: EventListenersView 
-    },
-    {
-      path: '/FromBindings',
-      name: 'Form Bindings',
-      component: FormBindingsView 
-    },
-  ]
+export const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/DeclarativeRendering',
+    name: 'Declarative Rendering',
+    component: DeclarativeRenderingView
+  },
+  {
+    path: '/AttributeBindings',
+    name: 'Attribute Bindings',
+    component: AttributeBindingsView
+  },
+  {
+    path: '/EventListeners',
+    name: 'Event Listeners',
+    component: EventListenersView
+  },
+  {
+    path: '/FromBindings',
+    name: 'Form Bindings',
+    component: FormBindingsView
+  },
+  {
+    path: '/ConditionalRendering',
+    name: 'Conditional Rendering',
+    component: ConditionalRenderingView 
+  },
+]
