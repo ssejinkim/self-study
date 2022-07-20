53d796b30d3931dbc0e2fc08f51faaccabcefd6a
Sejin Kim <sejinkim2022@gmail.com>
2022-07-20 11:19:02 +0900

    add Declarative Rendering example

---
+++ vue/vue-examples/src/views/DeclarativeRendering.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  import { reactive, ref } from 'vue'
+
+  const counter = reactive({
+    count : 0
+  })
+
+  console.log('before', counter.count)
+  counter.count++
+  console.log('after', counter.count)
+
+  const message = ref('hello world')
+
+  console.log('before', message.value)
+  message.value = 'hello, ref'
+  console.log('after', message.value)
+
+</script>
+
+<template>
+  <div>
+    <h1>Declarative Rendering</h1>
+    <h2>counter is: {{counter.count}}</h2>
+    <h2>{{message}}</h2>
+    <h2>{{message.split('').reverse().join('')}}</h2>
+  </div>
+</template>
